191b03e3b9504b248391ebfef98b6741de574a11
Gerson <geric99997@gmail.com>
2023-10-07 17:04:58 -0600

    add styling, responsive, logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'index-dashboard': "url('./vr.jpg')",
+        'logo': "url('./Spatial.Edu.png",
+        'index-dashboard-mobile': "url('./vr.jpg')",
+        'index-dashboard-desktop': "url('./vr.jpg')"
+        
       },
       height: {
        'image-size': '50%'
